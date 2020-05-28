248c83072f9f977eb3aa0afa7bad6c42fa08e55c
精武陈真 <546369005@qq.com>
2020-05-28 23:56:08 +0800

    feat: writeAllSync

---
+++ demo/writeAllSync/index.ts
@@ -0,0 +1,15 @@
+// Example writing to stdout
+const contentBytes = new TextEncoder().encode("Hello World");
+Deno.writeAllSync(Deno.stdout, contentBytes);
+
+// Example writing to file
+// const contentBytes = new TextEncoder().encode("Hello World");
+// const file = Deno.openSync('test.file', {write: true});
+// Deno.writeAllSync(file, contentBytes);
+// Deno.close(file.rid);
+
+// Example writing to buffer
+// const contentBytes = new TextEncoder().encode("Hello World");
+// const writer = new Deno.Buffer();
+// Deno.writeAllSync(writer, contentBytes);
+// console.log(writer.bytes().length);  // 11
