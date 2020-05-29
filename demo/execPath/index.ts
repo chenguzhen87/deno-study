dd35e21dcc9373419136451547e5660f96c1aa20
精武陈真 <546369005@qq.com>
2020-05-29 17:49:40 +0800

    feat:exePath

---
+++ demo/execPath/index.ts
@@ -0,0 +1,4 @@
+// Returns the path to the current deno executable.
+// Requires allow-read permission.
+// deno run --allow-read index.ts
+console.log(Deno.execPath()); // C:\Users\54636\.deno\bin\deno.exe
