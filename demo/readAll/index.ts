92220a13c082e9b356f28d152ae9eb16cd5f7804
精武陈真 <546369005@qq.com>
2020-05-31 21:33:38 +0800

    feat: readAll

---
+++ demo/readAll/index.ts
@@ -0,0 +1,16 @@
+// Example from stdin
+const stdinContent = await Deno.readAll(Deno.stdin);
+console.log(stdinContent);
+
+// Example from file
+const file = await Deno.open("my_file.txt", {read: true});
+const myFileContent = await Deno.readAll(file);
+console.log(myFileContent);
+Deno.close(file.rid);
+
+// Example from buffer
+const myData = new Uint8Array(100);
+// ... fill myData array with data
+const reader = new Deno.Buffer(myData.buffer as ArrayBuffer);
+const bufferContent = await Deno.readAll(reader);
+console.log(bufferContent);
