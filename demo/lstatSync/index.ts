17e0b17a622b95210485f948cf2d2d9502cf9556
精武陈真 <546369005@qq.com>
2020-05-31 18:35:39 +0800

    feat: lstatSync

---
+++ demo/lstatSync/index.ts
@@ -0,0 +1,7 @@
+// Resolves to a Deno.FileInfo for the specified path. If path is a symlink, information for the symlink will be returned instead of what it points to.
+// Requires allow-read permission.
+// deno run --allow-read index.ts
+const fileInfo = Deno.lstatSync("hello.txt");
+console.log(fileInfo.isFile); // true
+console.assert(fileInfo.isFile);
+ 
